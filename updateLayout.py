851ea759f5b26c45af81171c1c74fb567e16f8f6
shiedman <shiedman@gmail.com>
2012-11-07 14:03:46 +0000

    update init

--- updateLayout.py
+++ updateLayout.py
@@ -14,12 +14,12 @@ title='DOTCLOUD_PROJECT'
 #out.close()
 with open('views/layout.jade','rb') as f:lines=f.read()
 out=open('views/layout.jade','wb')
-founded=0
+#founded=0
 for line in lines.splitlines(True):
     if line.find('y2proxy_')>0 and title in os.environ:
         line=line.replace('y2proxy_','y2proxy@'+os.environ[title])
-    if founded==0 and line.find('nav.dotcloud')>=0: founded=1; continue
-    if founded>=1 and founded <=3:founded+=1;continue
+    #if founded==0 and line.find('nav.dotcloud')>=0: founded=1; continue
+    #if founded>=1 and founded <=3:founded+=1;continue
     out.write(line)
 out.close()
 
